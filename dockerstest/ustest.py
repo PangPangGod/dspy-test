933b6fb63b6f0cce65c75c75d0a2e42a74bca36f
PangPangGod <rhcp1134@gmail.com>
2024-04-11 04:17:25 +0900

    change internal method only wo

--- dockerstest/ustest.py
+++ dockerstest/ustest.py
@@ -1,15 +1,17 @@
+## reminder : currently only works in unstructured_client==0.22.0
 ## Unstructured api test
 from unstructured_client import UnstructuredClient
 from unstructured_client.models import shared
 
-api_key = "your_api_key"
+import time
+start_time = time.time()
 
 s = UnstructuredClient(
     server_url="http://localhost:8000",
-    api_key_auth=api_key,
+    api_key_auth="",
 )
 
-filename = "dockerstest/pdf/sample_file.pdf"  # 전송할 파일의 경로
+filename = ""  # 전송할 파일의 경로
 
 with open(filename, "rb") as f:
     files = shared.Files(
@@ -21,10 +23,15 @@ req = shared.PartitionParameters(
     files=files,
     chunking_strategy="by_title",
     strategy='hi_res',
+    split_pdf_page=True,
 )
 
 try:
     resp = s.general.partition(req)
-    print("처리된 요소:", resp.elements)
+    print("처리된 요소:", len(resp.elements))
 except Exception as e:
     print("오류 발생:", e)
+
+end_time = time.time()
+
+print(f"Execution time: {end_time - start_time} seconds")
